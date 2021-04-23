701fb919bec32f0b19e17fced178ac5d94d7d026
ingnicolaspj <68884962+ingnicolaspj@users.noreply.github.com>
2021-04-22 21:32:13 -0500

    Add files via upload

---
+++ 08042021/miprimerscrip.js
@@ -0,0 +1,41 @@
+//Un programa que le de la bienvenida a un usuario primero asignandolo a una varible y otro programa que le pida al usuario su nombre y le de la bienvenida.
+
+let pedirNombre = prompt("Dime tu nombre","Escribre tu nombre aquí");
+let pedirEdad = prompt("Dime tu edad",0);
+alert("Bienvenido a mi página web "+pedirNombre+" tu edad es : "+pedirEdad+" años");
+// let nombreCompleto = "Osvaldo Ortega";
+//     nombreCompleto = "Brian Cruz";
+
+// document.write("Bienvenido a mi página web "+nombreCompleto);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Con let defino mi variable le puedo asignar un valor al momento o despues en cualquier parte de mi códigos
+// let nombre;
+// let apellido;
+// let nombreYapellido;
+
+// nombre = "Guadalupe";
+// apellido = "Ortega";
+
+// nombreYapellido = nombre + apellido;
+
+// este codigo es para mostrar en consola y en pantalla mis varibales
+    
+
+// console.log(nombreYapellido);
+// document.write(nombreYapellido);
+// alert(nombreYapellido);
+
---
+++ 15042021/app.js
@@ -0,0 +1,67 @@
+//Operadores de asignación
+
+// let name = "Gudalupe";
+// let number = 8;
+
+// number /= 10;
+// number = number / 10;
+
+// alert(number);
+
+//Operadores aritmeticos
+
+// let number1 = 12;
+// //Los string con cadena de caracteres texto
+// let number2 = 3;
+
+// --number1;
+
+
+// let resultado1 = number1 + number2;
+// let resultado2 = number1 - number2;
+// let resultado3 = number1 * number2;
+// let resultado4 = number1 % number2;
+
+
+// document.write(resultado1+"<br>");
+// document.write(resultado2+"<br>");
+// document.write(resultado3+"<br>");
+// document.write(resultado4);
+
+
+
+//Operadores de comparación(Booleanos true o false)
+
+// let numero1 = 8;
+// let numero2 = 8;
+
+// let res = numero1 === numero2;
+
+// document.write(res);
+
+// let numero1 = 8;
+// let numero2 = "8";
+
+// let res = numero1 !== numero2;
+
+// document.write(res);
+
+// let numero1 = 8;
+// let numero2 = 15;
+
+// let res = numero1 >= numero2;
+
+// document.write(res);
+
+//Operadores logicos: Nos devuelce un resultado a partir de que cumpla o no una conción, su resulutado es booleano y sus operandos son valores logicos o asimilables a ellos
+
+let valor1 = 12;
+let valor2 = 24;
+        //true                  true
+let res = valor1 <= valor2 || valor1 >= valor2;
+
+document.write(res);
+
+
+
+
