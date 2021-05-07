d3dafef0052a5f8c553bfca643278896a4982d3e
ingnicolaspj <68884962+ingnicolaspj@users.noreply.github.com>
2021-05-06 19:55:55 -0500

    strings

---
+++ 06052021/string.js
@@ -0,0 +1,42 @@
+// console.log('Nico');
+// console.error('No se mandaron los datos al servidor');
+// console.warn('Cuidado estas entrando en un sitio web sin protocolos de seguridad');
+// console.clear();
+// console.warn('Todo esta ok');
+// console.time();
+
+// const producto = 'Monitor de 20 Pulgadas ';
+// const precio = '2500 pesos';
+
+// console.log(producto);
+// //.length obtenemos el numero de caracteres
+// console.log(producto.length);
+
+// console.log(producto.indexOf('Monitor'));
+// console.log(producto.includes('Pulgadas'));
+
+// //Tres formas de concatenar mis strings
+// console.log(producto.concat(precio));
+// console.log(producto + precio);
+// console.log(`El producto ${producto} tiene un precio de $${precio} `);
+
+// const tweet = "         Estamos aprendiendo JavaScript      ";
+// console.log(tweet);
+// console.log(tweet.length);
+
+// console.log(tweet.trimStart());
+// console.log(tweet.trimEnd());
+// console.log(tweet.trimStart().trimEnd());
+
+// console.log(tweet.length);
+
+const producto = 'Monitor de 20 Pulgadas ';
+
+console.log(producto.toUpperCase());
+console.log(producto.toLowerCase());
+
+const email = 'CORREO@GMAIL.COM';
+
+console.log(email.toLowerCase());
+
+console.log(producto.replace('Monitor','Pantalla'));
